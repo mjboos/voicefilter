1edb049ad4e0082fc7f09c6526ec59f6bf4d09d2
mjboos <moritz.boos@gmail.com>
2020-08-18 22:24:02 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="voicefilter",
+    version="0.0.1",
+    author="Moritz Boos",
+    author_email="moritz.boos@gmail.com",
+    packages=find_packages(),
+)
